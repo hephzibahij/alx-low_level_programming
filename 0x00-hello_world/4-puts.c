33d6fdfc88f7d6b06ab607a46c2bcd09352771d9
Hephzibah <hephzibahijeje89@gmail.com>
2023-04-20 06:59:21 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio h>
+#include <stdio.h>
 /**
- * main - A program that print a line with puts functio
-n
- * Return: 0 (Success)
+ * main - Entry
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}	
+
+	puts("\"Programming is like building a multilingual puzzle");
+	 return (0);
+}	 
