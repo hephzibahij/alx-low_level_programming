90a75c783367473b0b2ef992b236ee3ee23331c5
Hephzibah <hephzibahijeje89@gmail.com>
2023-04-22 02:18:58 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int n;
 
-	strand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
@@ -26,3 +26,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
+}
