65bb9d3d0f97145edad6c3f26d3954ff8d700787
Hephzibah <hephzibahijeje89@gmail.com>
2023-06-01 16:20:33 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+void first(void)__attribute__((constructor));
+
 /**
  * first - print the first statement befor the main
  * let the function be complied
  *
  */
-void first(void)__attribute__((constructor));
 void first(void)
 {
-	printf("You're best! and yet you must allow,\n");
+	printf("You're beat! and yet you must allow,\n");
 	printf("I bored my house upon my back!\n");
 }
