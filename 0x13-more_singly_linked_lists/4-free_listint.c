41e3d57099d8edc3924a4959363927a98cf2d482
Hephzibah <hephzibahijeje89@gmail.com>
2023-06-06 17:13:23 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - note the free linked element in the listint
+ * @head: wite the freed listint
+ *
+ * Return: 0
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (temp)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
