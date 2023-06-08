e092dfc16225fd04f5562525a002d40549cc7724
Hephzibah <hephzibahijeje89@gmail.com>
2023-06-08 16:36:05 -0700

    Task 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - select and print all the eklement in the binary number
+ * @n: pick the number of binary numbers to print
+ * Return: 0
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int current;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
