96a3be264f9f6b5b0ab0b6cb1faaf74fe39027a8
Hephzibah <hephzibahijeje89@gmail.com>
2023-08-18 14:08:46 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 	current = *h;
-
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (!current)
@@ -44,13 +43,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (!current)
 		return (NULL);
-
 	new_node->next = current->next;
 	new_node->prev = current;
-
 	if (current->next)
 		current->next->prev = new_node;
-
 	current->next = new_node;
 	return (new_node);
 }
