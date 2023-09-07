d1882684026d542813b995ad3b7c99445e85ced7
Hephzibah <hephzibahijeje89@gmail.com>
2023-09-07 17:18:22 -0400

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_node, *temp;
+	hash_node_t *new_node, *head;
 	char *dup_value = (value == NULL) ? NULL : strdup(value);
 
 	if (ht == NULL || key == NULL || *key == '\0')
@@ -37,15 +37,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->value = dup_value;
 	new_node->next = NULL;
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-	}
-	else
-	{
-		temp = ht->array[index];
-		new_node->next = temp;
-		ht->array[index] = new_node;
-	}
+	head = ht->array[index];
+	ht->array[index] = new_node;
+	new_node->next = head;
 	return (1);
 }
