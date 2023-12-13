9f0abef6b204fe585cbc284c622d8d293ca2e157
Hephzibah <hephzibahijeje89@gmail.com>
2023-12-13 14:30:34 -0800

    Tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,13 +17,14 @@ int linear_search(int *array, size_t size, int value)
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; ++i)
+
+	for (i = 0; i < size; i++)
 	{
-		printf("Comparing %d with %d\n", array[i], value);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
 		}
 	}
-	return  (-1);
+	return (-1);
 }
